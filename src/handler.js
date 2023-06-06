ea33dd91908895d572b5bed7822a94bfbeead216
mhmmdputera <126052984+mhmmdputera@users.noreply.github.com>
2023-06-05 18:00:26 -0800

    Update handler.js

--- src/handler.js
+++ src/handler.js
@@ -15,52 +15,56 @@ const addBookHandler = (request, h) => {
       message: 'Gagal menambahkan buku. Mohon isi nama buku',
     });
     response.code(400);
+
     return response;
-  } else if (readPage > pageCount) {
+  }
+
+  if (readPage > pageCount) {
     const response = h.response({
       status: 'fail',
       // eslint-disable-next-line max-len
       message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
     });
     response.code(400);
+
     return response;
-  } else {
-    const id = nanoid(16);
-    const insertedAt = new Date().toISOString();
-    const updatedAt = insertedAt;
-    const finished = (pageCount === readPage);
-
-    const newBook = {
-      id,
-      name,
-      year,
-      author,
-      summary,
-      publisher,
-      pageCount,
-      readPage,
-      finished,
-      reading,
-      insertedAt,
-      updatedAt,
-    };
-
-    books.push(newBook);
-
-    const isSuccess = books.filter((book) => book.id === id).length > 0;
-
-    if (isSuccess) {
-      const response = h.response({
-        status: 'success',
-        message: 'Buku berhasil ditambahkan',
-        data: {
-          bookId: id,
-        },
-      });
-      response.code(201);
-      return response;
-    }
   }
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const finished = (pageCount === readPage);
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt,
+  };
+
+  books.push(newBook);
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
   const response = h.response({
     status: 'fail',
     message: 'Buku gagal ditambahkan',
